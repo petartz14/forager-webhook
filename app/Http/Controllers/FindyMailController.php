05b71ea23700ac07b31adea28a0304b406dd0f71
efren0214 <efren@claritystack.com>
2023-09-20 21:24:11 +0800

    webhook test

---
+++ app/Http/Controllers/FindyMailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class FindyMailController extends Controller
+{
+    public function __invoke()
+    {
+        header('Content-Type: application/json');
+
+        $request = file_get_contents('php://input');
+        $payload = json_decode($request, true)['data'];
+
+        Log::info('Received Findymail webhook.', $payload);
+
+    }
+}
