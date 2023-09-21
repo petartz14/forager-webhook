2c77dced280cccbb80ba378e0f8bf6922c2ed11a
efren0214 <efren@claritystack.com>
2023-09-21 16:43:44 +0800

    fixed

--- app/Http/Controllers/FindyMailController.php
+++ app/Http/Controllers/FindyMailController.php
@@ -10,12 +10,7 @@ class FindyMailController extends Controller
 {
     public function __invoke()
     {
-        WebhookLog::create(['name' => 'Findymail-1', 'data' => ['name' => 'test']]);
-
         $request = file_get_contents('php://input');
-
-        WebhookLog::create(['name' => 'Findymail', 'data' => ['data' => $request]]);
-
         $payload = json_decode($request, true);
 
         WebhookLog::create(['name' => 'Findymail', 'data' => $payload]);
